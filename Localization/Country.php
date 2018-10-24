94a5a0c64973334d9251a56e70922590322a2fec
david <d.marsalone@gmail.com>
2018-10-24 13:51:19 +0200

    Country class

---
+++ Localization/Country.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Localization;
+
+class Country{
+    /**
+     * @var String The iso code (FR for France, etc...)
+     */
+    public $code;
+    /**
+     * @var String The name of this country in english.
+     */
+    public $name;
+
+    /**
+     * @var Country[] all countries indexed by codes
+     */
+    public static $ALL=[];
+
+
+    /**
+     * Country constructor.
+     * @param $code
+     * @param $name
+     */
+    public function __construct($code,$name)
+    {
+        $this->code=$code;
+        $this->name=$name;
+    }
+
+    /**
+     * Return full list countries indexed by codes.
+     * @return Country[]
+     */
+    public static function allFromJson($asArray=false){
+        if(!self::$_allFromJson){
+            $array=json_decode(file_get_contents(__DIR__."/../countries.json"));
+            $return=[];
+            foreach ($array as $c){
+                $c=new Country($c->{'alpha-2'},$c->name);
+                $return[$c->code]=$c;
+            }
+            self::$_allFromJson=$return;
+        }
+
+        if($asArray){
+            $r=[];
+            foreach (self::$_allFromJson as $c){
+                $r[$c->name]=$c->code;
+            }
+            return $r;
+        }
+
+        return self::$_allFromJson;
+    }
+
+
+
+    /**
+     * @var Country[] cache for allFromJson method.
+     */
+    private static $_allFromJson;
+
+}
